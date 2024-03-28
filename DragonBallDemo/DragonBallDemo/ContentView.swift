57035e4d9eba49a15f5612ff70386b4f200d9f20
Rafael Escaleira <rafaelescaleira@icloud.com>
2024-03-28 01:33:36 -0400

    Add demo project

---
+++ DragonBallDemo/DragonBallDemo/ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+import DragonBallUIKit
+
+struct ContentView: View {
+    var body: some View {
+        CharactersView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DragonBallDemo/DragonBallDemo/DragonBallDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  DragonBallDemoApp.swift
+//  DragonBallDemo
+//
+//  Created by Rafael Santos on 28/03/24.
+//
+
+import SwiftUI
+
+@main
+struct DragonBallDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .tint(.orange)
+        }
+    }
+}
